29d067b04cabf3ad3cae83fd8de6d7068476d94a
onspace.ai <onspace.ai@gmail.com>
2025-08-07 19:52:14 +0200

    Fix Colors import in ErrorBoundary

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Button } from 'react-native-paper';
 import { MaterialIcons } from '@expo/vector-icons';
-import { Colors } from '../constants/Colors';
+import Colors from '../constants/Colors';
 import { Typography } from '../constants/Typography';
 
 interface ErrorBoundaryState {
